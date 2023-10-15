d34074b16af115441c254e8ca56081568b3ab84a
Robert Vari <mail.robertvari@gmail.com>
2023-10-15 14:52:36 +0200

    Logo

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import Logo from "../images/logo.svg"
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <div>
+        <img src={Logo} alt="" />
+    </div>
   )
 }
