ebdecd8ef457ad2abbcea852e0992138b4d38b9a
Robert Vari <mail.robertvari@gmail.com>
2023-10-15 14:42:29 +0200

    Navbar, MovieListView, Footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
+import Footer from "./components/Footer";
+import MovieListView from "./components/MovieListView";
+import Navbar from "./components/Navbar";
 import "./styles/main.css"
 
 function App() {
   return (
     <div className="App">
-      <h1>The Movie Database</h1>
-      <h2>The Movie Database</h2>
-      <h3>The Movie Database</h3>
-
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur non aspernatur ullam explicabo voluptatem soluta, sunt pariatur magni dolores nemo laborum numquam, magnam facere ipsum ad a! Amet labore ipsa ut illum, non delectus totam aut, animi a, consequuntur deleniti omnis! Recusandae nobis, nemo temporibus totam magnam, dolores deleniti fugit ipsum commodi quo dicta quidem laborum deserunt, neque labore vero similique rem quae! Soluta nam itaque rem fugiat tenetur, ad voluptatem quibusdam officia asperiores tempore expedita quas blanditiis animi voluptatibus provident architecto consectetur repellendus a fugit. Dicta eaque odit et. Asperiores excepturi dolorem beatae culpa pariatur qui recusandae vitae nam!</p>
+      <Navbar/>
+      <MovieListView/>
+      <Footer/>
     </div>
   );
 }
