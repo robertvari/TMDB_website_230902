eff56469c53c9d85f0e446b61a52540bc1614403
Robert Vari <mail.robertvari@gmail.com>
2023-10-15 15:32:56 +0200

    Navbar layour finished

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,15 +4,19 @@ import Logo from "../images/logo.svg"
 export default function Navbar() {
   return (
     <div className='navbar'>
-        <img  src={Logo} alt="" />
-        <div className='navlink'>Movies</div>
-        <div className='navlink'>TV Shows</div>
-        <div className='navlink'>People</div>
-        <div className='navlink'>More</div>
+        <div className='nav-block'>
+            <img  src={Logo} alt="" />
+            <div className='navlink'>Movies</div>
+            <div className='navlink'>TV Shows</div>
+            <div className='navlink'>People</div>
+            <div className='navlink'>More</div>
+        </div>
 
-        <div className='navlink'>Login</div>
-        <div className='navlink'>Join TMDB</div>
-        <i className="fa fa-search" aria-hidden="true"/>
+        <div className='nav-block'>
+            <div className='navlink'>Login</div>
+            <div className='navlink'>Join TMDB</div>
+            <i className="fa fa-search" aria-hidden="true"/>
+        </div>
     </div>
   )
 }
