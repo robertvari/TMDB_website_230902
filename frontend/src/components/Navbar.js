0ba141bd4e5395297c0ec7e948032659ce767521
Robert Vari <mail.robertvari@gmail.com>
2023-10-15 15:19:34 +0200

    Include fontawesome

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,8 +3,16 @@ import Logo from "../images/logo.svg"
 
 export default function Navbar() {
   return (
-    <div>
-        <img src={Logo} alt="" />
+    <div className='navbar'>
+        <img  src={Logo} alt="" />
+        <div className='navlink'>Movies</div>
+        <div className='navlink'>TV Shows</div>
+        <div className='navlink'>People</div>
+        <div className='navlink'>More</div>
+
+        <div className='navlink'>Login</div>
+        <div className='navlink'>Join TMDB</div>
+        <i className="fa fa-search" aria-hidden="true"/>
     </div>
   )
 }
