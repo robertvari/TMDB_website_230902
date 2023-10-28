ede4d09e342700bd2646df7d9c66efcd0d69cb60
Robert Vari <mail.robertvari@gmail.com>
2023-10-28 16:06:23 +0200

    update

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import Logo from "../images/logo.svg"
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
     <div className='navbar'>
         <div className='nav-block'>
-            <img  src={Logo} alt="" />
-            <div className='navlink'>Movies</div>
-            <div className='navlink'>TV Shows</div>
-            <div className='navlink'>People</div>
-            <div className='navlink'>More</div>
+            <Link to="/"><img  src={Logo} alt="" /></Link>
+            <Link to="/" className='navlink'>Movies</Link>
+            <Link to="/tvshows" className='navlink'>TV Shows</Link>
+            <Link to="/people" className='navlink'>People</Link>
+            <Link to="/more" className='navlink'>More</Link>
         </div>
 
         <div className='nav-block'>
-            <div className='navlink'>Login</div>
-            <div className='navlink'>Join TMDB</div>
+            <Link to="/login" className='navlink'>Login</Link>
+            <Link to="/registration" className='navlink'>Join TMDB</Link>
             <i className="fa fa-search" aria-hidden="true"/>
         </div>
     </div>
