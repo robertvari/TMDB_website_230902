4ae14e021cae0f697b487e9569b36219906bd39d
Robert Vari <mail.robertvari@gmail.com>
2023-10-28 15:58:19 +0200

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,38 @@
 import Footer from "./components/Footer";
 import MovieListView from "./components/MovieListView";
+import TVShowsView from "./components/TVShowsView";
+import PeopleView from "./components/PeopeView";
+import MoreView from "./components/MoreView";
+import LoginPage from "./components/LoginPage";
+import RegistrationPage from "./components/RegistrationPage";
+import NotFoundPage from "./components/NotFoundPage"
 import Navbar from "./components/Navbar";
 import "./styles/main.css"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <Navbar/>
-        <MovieListView/>
-      </div>
-
+      <BrowserRouter>
       
-      <Footer/>
+        <div>
+          <Navbar/>
+
+          <Routes>
+            <Route path="/tvshows" element={<TVShowsView/>}/>
+            <Route path="/people" element={<PeopleView/>}/>
+            <Route path="/more" element={<MoreView/>}/>
+            <Route path="/login" element={<LoginPage/>}/>
+            <Route path="/registration" element={<RegistrationPage/>}/>
+            
+            <Route path="/" element={<MovieListView/>}/>
+            <Route path="*" element={<NotFoundPage/>}/>
+          </Routes>
+        </div>
+        
+        <Footer/>
+
+      </BrowserRouter>
     </div>
   );
 }
