b9bbca46761c5063ce8565ea0b33bdbc11a3906a
Robert Vari <mail.robertvari@gmail.com>
2023-10-28 17:43:05 +0200

    update

--- frontend/src/components/RegistrationPage.js
+++ frontend/src/components/RegistrationPage.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function RegistrationPage() {
   return (
-    <h1>RegistrationPage</h1>
+    <div className='content-container registration-container'>
+      <h2>Sign up for an account</h2>
+      <p>Signing up for an account is free and easy. Fill out the form below to get started. JavaScript is required to to continue.</p>
+
+      <form action="">
+        <input type="text" placeholder='Username'/>
+        <input type="password" placeholder='Password'/>
+        <input type="password" placeholder='Password Confirm'/>
+        <input type="email" placeholder='Email'/>
+
+        <div>
+          <button>Sign Up</button>
+          <Link to="/">Cancel</Link>
+        </div>
+      </form>
+    </div>
   )
 }
