29b67394e2f61028dc19b1684f434a5c434b5f31
Robert Vari <mail.robertvari@gmail.com>
2023-10-28 15:34:20 +0200

    update

---
+++ frontend/src/components/MoreView.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MoreView() {
+  return (
+    <h1>MoreView</h1>
+  )
+}
---
+++ frontend/src/components/PeopeView.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function PeopeView() {
+  return (
+    <h1>PeopeView</h1>
+  )
+}
