0b4a9a48d160064eb45615f4544b8d41c3c0b7a2
Robert Vari <mail.robertvari@gmail.com>
2023-10-29 18:15:17 +0100

    update

--- frontend/src/components/CardGrid.js
+++ frontend/src/components/CardGrid.js
@@ -1,24 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import MovieCard from './micro/MovieCard'
+import axios from 'axios'
 
 export default function CardGrid() {
+  const API_URL = process.env.REACT_APP_API_URL
+  const [movie_list, set_movie_list] = useState([])
+
+
+  useEffect(() => {
+    axios.get(`${API_URL}/api/movie-list/`).then(response => set_movie_list(response.data))
+  }, [])
+
   return (
     <div className='card-grid'>
-      <MovieCard/>
-      <MovieCard/>
-      <MovieCard/>
-      <MovieCard/>
-      <MovieCard/>
-      <MovieCard/>
-      <MovieCard/>
-      <MovieCard/>
-      <MovieCard/>
-      <MovieCard/>
-      <MovieCard/>
-      <MovieCard/>
-      <MovieCard/>
-      <MovieCard/>
-      <MovieCard/>
+      {
+        movie_list.map(movie_data => <MovieCard key={movie_data.id} movie_data={movie_data}/>)
+      }
     </div>
   )
 }
--- frontend/src/components/micro/MovieCard.js
+++ frontend/src/components/micro/MovieCard.js
@@ -2,17 +2,17 @@ import React from 'react'
 import MoviePoster from "../../images/poster.jpg"
 import {Link} from "react-router-dom"
 
-export default function MovieCard() {
+export default function MovieCard({movie_data}) {
   return (
     <Link to="/movies/123" className='movie-card'>
         <div className='poster-container'>
-            <img src={MoviePoster} alt="" />
+            <img src={movie_data.poster_path} alt="" />
             <div className='rating-container'>59%</div>
         </div>
 
         <div className='info-container'>
-            <div className='title'>The Exorcist: Believer</div>
-            <div className='date'>Oct 06, 2023</div>
+            <div className='title'>{movie_data.title}</div>
+            <div className='date'>{movie_data.release_date}</div>
         </div>    
     </Link>
   )
